2a4b876fd58f6b0aca647f4c0badc614afd3b79a
Daniel Uzcategui <danieluzcategui@Daniels-MacBook-Pro.local>
2026-04-02 21:45:41 -0400

    Updating files into dev, solved issues of branching

--- services/recommender/embedder.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-class Embedder:
-    def __init__(self, model="all-MiniLM-L6-v2"):
-        self.model = SentenceTransformer(model)
-
-    def create_embedding(self, item):
-        embeddings = self.model.encode(item)
-        return embeddings
